afa25ca8c5dd39a0f07bb9c8d76bc4c89e70c8bf
Raj <yadavrajkaran854@gmail.com>
2026-01-01 10:00:00 +0530

    Initial commit: Rebranded 3D Portfolio by Raj

---
+++ sources/data/social.js
@@ -0,0 +1,10 @@
+export default [
+    { name: 'X', url: 'https://x.com/Raj_simon', align: 'right' },
+    { name: 'Bluesky', url: 'https://bsky.app/profile/Raj-simon.bsky.social', align: 'right' },
+    { name: 'Youtube', url: 'https://www.youtube.com/@RajSimon', align: 'right' },
+    { name: 'Mail', url: 'mailto:simon.Raj.77@gmail.com', align: 'right' },
+    { name: 'Twitch', url: 'https://www.twitch.tv/Raj_simon_dev', align: 'right' },
+    { name: 'GitHub', url: 'https://github.com/Rajsimon', align: 'right' },
+    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/simonRaj77/', align: 'left' },
+    { name: 'Discord', modal: 'discord', align: 'left' },
+]
